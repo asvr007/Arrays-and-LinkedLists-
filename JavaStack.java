7de505d444d2f46c1d00b28eb1686b2883a4a324
Adem Sreenivasula Reddy <sreenureddyadem@gmail.com>
2023-07-23 18:55:34 +0530

    Create JavaStack.java

---
+++ JavaStack.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+
+
+    public static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String input = sc.next();
+            boolean balanced = isBalanced(input);
+            System.out.println(balanced);
+        }
+
+        sc.close();
+    }
+}
